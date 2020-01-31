4aa87974b0fe70e76391458455626c3970e9143b
Laurentino Vilchez <laurentino.vilchez@gmail.com>
2020-01-31 20:22:35 +0100

    proyecto base

---
+++ Source/RepairJam_/RepairJam_.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "RepairJam_.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, RepairJam_, "RepairJam_" );
